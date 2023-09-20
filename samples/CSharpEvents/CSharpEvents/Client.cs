45844114bdb814042cf6227ffe244676320e0d66
Dennis <dvdstelt@gmail.com>
2023-09-20 12:40:48 +0200

    Simple C# in-process events

---
+++ samples/CSharpEvents/CSharpEvents/Client.cs
@@ -0,0 +1,13 @@
+﻿public class Client
+{
+    int calculationsDone = 0;
+    public event EventHandler ThresholdReached;
+
+    public int Add(int x, int y)
+    {
+        if (++calculationsDone == 3)
+            ThresholdReached?.Invoke(this, EventArgs.Empty);
+
+        return x + y;
+    }
+}
---
+++ samples/CSharpEvents/CSharpEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Press [ENTER] to execute");
+
+var calc = new Client();
+var rnd = new Random();
+
+#region First event handler
+// calc.ThresholdReached += CalcOnThresholdReached;
+//
+// void CalcOnThresholdReached(object? sender, EventArgs eventArgs)
+// {
+//     Console.WriteLine("An event was just fired!");
+// }
+#endregion
+
+#region Second event handler
+// calc.ThresholdReached += delegate
+// {
+//     Console.WriteLine("We've done enough calculations, I'm quitting");
+//     Environment.Exit(0);
+// };
+#endregion
+
+while (true)
+{
+    if (!Console.KeyAvailable) continue;
+    var userInput = Console.ReadKey(true);
+
+    var result = calc.Add(1295, rnd.Next(40,44));
+    Console.WriteLine($"The result is: {result}");
+}
