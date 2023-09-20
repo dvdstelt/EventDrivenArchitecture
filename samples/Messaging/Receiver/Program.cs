670f3fe8181f1b7f2272e85a58712bb307fd2a03
Dennis <dvdstelt@gmail.com>
2023-09-20 17:03:04 +0200

    Cleaned up another file

--- samples/Messaging/Receiver/Program.cs
+++ samples/Messaging/Receiver/Program.cs
@@ -1,9 +1,2 @@
-﻿var config = new EndpointConfiguration("Receiver");
-config.UseTransport(new LearningTransport());
-
-var endpoint = await Endpoint.Start(config);
-
-Console.WriteLine("Waiting for messages...");
+﻿Console.WriteLine("Waiting for messages...");
 Console.ReadKey();
-
-await endpoint.Stop();
